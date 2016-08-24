1e2086c04e4e00e100cee5c8c1b3769b8f5631d4
Samantha Spensley <spenslsm@gmail.com>
2016-08-24 11:30:10 -0600

    so many handlebars

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express')
+let hbs = require('hbs')
 let app = express();
 
+//configuring express with handlebars
+app.set('view engine', 'hbs');
+hbs.localsAsTemplateData(app);
+
+app.locals.cohortName = 'g31'
+app.locals.title = 'My Handlebars Title'
+
+//registering routes
 app.get('/', (req, res) => {
-  res.json({message: 'Hello from express!'})
+  //render a template file and send back to client
+  res.render('home', {
+    cohortName: 'g31',
+    foods: [
+      'chalupa',
+      'ceviche',
+      'sprinkles'
+    ]
+  })
 })
 
 app.get('/page2', (req, res) => {
-  res.json({message: 'I am page 2!!!'})
+  res.render('page2')
 })
 
+//listening
 app.listen(3000, () => {
   console.log('Server runnin on port 3000');
 })
