be59b47ad08242a63bc406b978024fa58a769678
Samantha Spensley <spenslsm@gmail.com>
2016-08-24 10:41:54 -0600

    handlebars added

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+let app = express();
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello from express!'})
+})
+
+app.get('/page2', (req, res) => {
+  res.json({message: 'I am page 2!!!'})
+})
+
+app.listen(3000, () => {
+  console.log('Server runnin on port 3000');
+})
